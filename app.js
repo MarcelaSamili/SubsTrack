fc8bb71b971eec5f322ebefc9d4147c481712c94
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-02-13 09:52:18 -0300

    bug fixed

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import express from 'express';
 
 import { PORT } from './config/env.js';
 
-import userRouter from './routs/user.routes';
-import authRouter from './routs/auth.routes';
-import subscriptionRouter from './routs/subscription.routes';
+import userRouter from './routs/user.routes.js';
+import authRouter from './routs/auth.routes.js';
+import subscriptionRouter from './routs/subscription.routes.js';
 
 const app = express();
 
 app.use('/api/v1/auth', authRouter);
-app.use('api/v1/users', userRouter);
+app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscription', subscriptionRouter);
 
 app.get('/', (req, res) => {
